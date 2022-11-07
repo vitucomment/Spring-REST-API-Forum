b2ab6a99cc482231aa62589c25e2f7f7ac3b5705
vitucomment <victorpy.1999@gmail.com>
2022-11-07 15:19:03 -0300

    add bean validation on TopicoForm

--- src/main/java/br/com/alura/forum/controller/dto/TopicoForm.java
+++ src/main/java/br/com/alura/forum/controller/dto/TopicoForm.java
@@ -1,15 +1,25 @@
 package br.com.alura.forum.controller.dto;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.CursoRepository;
 
 public class TopicoForm {
 
-	private String titulo;
-	private String mensagem;
-	private String nomeCurso;
+	final String MESSAGE_TITULO = "Minimo de 5 characters";
+	final String MESSAGE_MENSAGEM = "Minimo de 10 characters";
 
+	@NotNull @NotEmpty @Size(min = 5, message = MESSAGE_TITULO)
+	private String titulo;
+	@NotNull @NotEmpty @Size(min = 10, message = MESSAGE_MENSAGEM)
+	private String mensagem;
+	@NotNull @NotEmpty
+	private String nomeCurso;
+	
 	
 	public String getTitulo() {
 		return titulo;
