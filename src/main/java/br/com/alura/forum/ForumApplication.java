1a1d1368382b08c321da9279fc76aad4bf522d19
vitucomment <victorpy.1999@gmail.com>
2022-11-08 18:31:55 -0300

    adicionando Spring Secutiry e restrigindo URLs

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -1,6 +1,6 @@
 package br.com.alura.forum;
 
-import org.springframework.boot.SpringApplication;
+ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
---
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -0,0 +1,34 @@
+package br.com.alura.forum.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SuppressWarnings("deprecation")
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+	//Configurações de autenticação
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	}
+
+	//Configurações de autorização
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
+		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+		.anyRequest().authenticated();
+	}
+
+	//Configurações de recursos estaticos
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+	}
+}
